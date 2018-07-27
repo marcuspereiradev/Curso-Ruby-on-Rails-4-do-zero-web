67b2243190209d53b941c008cedaa321bbca4afb
marcuspereiragithub <vinnycoast@gmail.com>
2018-07-27 16:42:38 -0300

    Adding controllers and routes of new.html.erb and adding destroy to remove button

--- lojadecamisetas/app/controllers/produtos_controller.rb
+++ lojadecamisetas/app/controllers/produtos_controller.rb
@@ -3,4 +3,16 @@ class ProdutosController < ApplicationController
       @produtos_por_nome = Produto.order(:nome).limit 5
       @produtos_por_preco = Produto.order(:preco).limit 2
     end
+
+    def create
+      valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco
+      produto = Produto.create valores
+      redirect_to root_url
+    end  
+
+    def destroy
+      id = params[:id]
+      Produto.destroy id
+      redirect_to root_url
+    end
 end
--- lojadecamisetas/config/routes.rb
+++ lojadecamisetas/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/produtos/new' => 'produtos#new'
+  resources :produtos, only: [:new, :create, :destroy]
   root 'produtos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
