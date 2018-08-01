41fa802620d6aa8cbaa82bdde23acf89db9b3b5b
marcuspereiragithub <vinnycoast@gmail.com>
2018-08-01 16:31:42 -0300

    Adding scaffold Departamento

--- lojadecamisetas/db/schema.rb
+++ lojadecamisetas/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_152449) do
+ActiveRecord::Schema.define(version: 2018_08_01_192454) do
+
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
