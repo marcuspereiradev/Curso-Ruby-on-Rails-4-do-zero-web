7113a8f2771c4541e0e1aace7e678c201b29f62f
marcuspereiragithub <vinnycoast@gmail.com>
2018-08-01 15:21:54 -0300

    Applying condition to action create

--- lojadecamisetas/app/controllers/produtos_controller.rb
+++ lojadecamisetas/app/controllers/produtos_controller.rb
@@ -6,9 +6,18 @@ class ProdutosController < ApplicationController
 
     def create
       valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco
-      produto = Produto.create valores
-      redirect_to root_url
+      @produto = Produto.new valores
+      if @produto.save
+        redirect_to root_url
+      else
+        render :new
+      end
     end  
+
+    def busca
+      @nome_a_buscar = params[:nome]
+      @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+    end
 
     def destroy
       id = params[:id]
