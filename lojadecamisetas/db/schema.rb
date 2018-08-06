e5d4f727013827fbd610fb4daf6f3922ae06bb7e
marcuspereiragithub <vinnycoast@gmail.com>
2018-08-06 14:30:56 -0300

    Adding integer departamento

--- lojadecamisetas/db/schema.rb
+++ lojadecamisetas/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_192454) do
+ActiveRecord::Schema.define(version: 2018_08_03_135417) do
 
   create_table "departamentos", force: :cascade do |t|
     t.string "nome"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_08_01_192454) do
     t.integer "quantidade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "departamento_id"
   end
 
 end
